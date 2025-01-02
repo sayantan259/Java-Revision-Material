e230ef6f0facdf634444c2ebbe7f666a93099a19
Sayantan Bairagi <sayantan259@gmail.com>
2025-01-02 23:19:24 +0530

    Methods Added

--- ClassAndObject.java
+++ ClassAndObject.java
@@ -6,6 +6,16 @@ class calculator{
     public int subtract(int n1,int n2){
         return n1 - n2;
     }
+    public int divide(int n1,int n2){
+        if(n2 == 0){
+            System.out.println("Can not divide by zero, returning zero from this function.");
+            return 0;
+        }
+        return n1 / n2;
+    }
+    public int multiply(int n1,int n2){
+        return n1 * n2;
+    }
 
     // Example of Overloading
     public double add(double n1,double n2){
